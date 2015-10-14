38fff8b994e27a638e7af8ef7ad6dbbfe917529b
Richard J Byrne <richard.j.Byrne@gmail.com>
2015-10-14 15:12:00 +0100

    Added quick modes for demo

--- Haptic Theatre-Vibings Control/Controllers/LiveController.cs
+++ Haptic Theatre-Vibings Control/Controllers/LiveController.cs
@@ -19,6 +19,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Haptic_Theatre_Vibings_Control.Classes;
 
 namespace Haptic_Theatre_Vibings_Control.Controllers
 {
@@ -29,5 +30,103 @@ namespace Haptic_Theatre_Vibings_Control.Controllers
         {
             return View();
         }
+
+
+        #region JUST FOR DEMOS
+
+        public ActionResult Mode_001(string colourHex)
+        {
+            HTTPManager.SendGetRequest("http://192.168.100.13/?mode=001&color=%23" + colourHex);
+            HTTPManager.SendGetRequest("http://192.168.100.12/?mode=001&color=%23" + colourHex);
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Mode_002(string colourHex, string loopDelay)
+        {
+            HTTPManager.SendGetRequest("http://192.168.100.13/?mode=002&color=%23" + colourHex + "&loopdelay=" + loopDelay);
+            HTTPManager.SendGetRequest("http://192.168.100.12/?mode=002&color=%23" + colourHex + "&loopdelay=" + loopDelay);
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Mode_003(string colourHex, string loopDelay)
+        {
+            HTTPManager.SendGetRequest("http://192.168.100.13/?mode=003&color=%23" + colourHex + "&loopdelay=" + loopDelay);
+            HTTPManager.SendGetRequest("http://192.168.100.12/?mode=003&color=%23" + colourHex + "&loopdelay=" + loopDelay);
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Mode_004(string colourHex, string loopDelay)
+        {
+            HTTPManager.SendGetRequest("http://192.168.100.13/?mode=004&color=%23" + colourHex + "&loopdelay=" + loopDelay);
+            HTTPManager.SendGetRequest("http://192.168.100.12/?mode=004&color=%23" + colourHex + "&loopdelay=" + loopDelay);
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Mode_005(string colourHex, string loopDelay)
+        {
+            HTTPManager.SendGetRequest("http://192.168.100.13/?mode=005&color=%23" + colourHex + "&loopdelay=" + loopDelay);
+            HTTPManager.SendGetRequest("http://192.168.100.12/?mode=005&color=%23" + colourHex + "&loopdelay=" + loopDelay);
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Mode_006(string colourHex, string loopDelay)
+        {
+            HTTPManager.SendGetRequest("http://192.168.100.13/?mode=006&color=%23" + colourHex + "&loopdelay=" + loopDelay);
+            HTTPManager.SendGetRequest("http://192.168.100.12/?mode=006&color=%23" + colourHex + "&loopdelay=" + loopDelay);
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Mode_007(string colourHex_01, string colourHex_02, string colourHex_03, string colourHex_04, string loopDelay)
+        {
+            HTTPManager.SendGetRequest("http://192.168.100.13/?mode=007" +
+                                       "&color01=%23" + colourHex_01 +
+                                       "&color02=%23" + colourHex_02 +
+                                       "&color03=%23" + colourHex_03 +
+                                       "&color04=%23" + colourHex_04 +
+                                       "&loopdelay=" + loopDelay);
+
+            HTTPManager.SendGetRequest("http://192.168.100.12/?mode=007" +
+                           "&color01=%23" + colourHex_01 +
+                           "&color02=%23" + colourHex_02 +
+                           "&color03=%23" + colourHex_03 +
+                           "&color04=%23" + colourHex_04 +
+                           "&loopdelay=" + loopDelay);
+
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Mode_008()
+        {
+            HTTPManager.SendGetRequest("http://192.168.100.13/?mode=008&color=%23");
+            HTTPManager.SendGetRequest("http://192.168.100.12/?mode=008&color=%23");
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Mode_009()
+        {
+            HTTPManager.SendGetRequest("http://192.168.100.13/?mode=009&color=%23");
+            HTTPManager.SendGetRequest("http://192.168.100.12/?mode=009&color=%23");
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Mode_010()
+        {
+            return View("Index");
+        }
+
+        public ActionResult Mode_011(string colourHex, string loopDelay)
+        {
+            HTTPManager.SendGetRequest("http://192.168.100.13/?mode=011&color=%23" + colourHex + "&loopdelay=" + loopDelay);
+            HTTPManager.SendGetRequest("http://192.168.100.12/?mode=011&color=%23" + colourHex + "&loopdelay=" + loopDelay);
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Mode_012()
+        {
+            return View("Index");
+        }
+
+        #endregion
     }
 }
