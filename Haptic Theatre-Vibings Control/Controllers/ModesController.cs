c0f0d2dacca5797c5e4c5f7783b7ee4dda30496c
Richard J Byrne <richard.j.Byrne@gmail.com>
2016-07-18 14:28:48 +0100

    Data for mode changes and pretend heart rate

---
+++ Haptic Theatre-Vibings Control/Controllers/ModesController.cs
@@ -0,0 +1,49 @@
+﻿#region File Information
+
+/********************************************************************************************
+* \Haptic Theatre-Vibings ControlX\Haptic Theatre-Vibings Control\Controllers\ModesController.cs
+* ****************************************************************************************** 
+ DESCRIPTION   : Used when setting up modes.  Where a mode is collection of colours, timings, fades etc 
+ that are sent to a vibing as a package and the vibings uses to determin what to display.
+
+ REVISION HISTORY: 
+
+ Date(MM/DD/YYYY)		    REV BY		           REV DESC
+ ----------------------------------------------------------------------------------------------
+ 11/09/2015                 Richard Byrne          Created
+ 28/09/2015                 Richard Byrne          Added new views  
+**********************************************************************************************/
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Haptic_Theatre_Vibings_Control.Controllers
+{
+    public class ModesController : Controller
+    {
+        // GET: Modes
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Modes
+        public ActionResult ModeForShows()
+        {
+            return View();
+        }
+
+        // GET: Modes
+        public ActionResult ModeDesign()
+        {
+            return View();
+        }
+
+
+    }
+}
