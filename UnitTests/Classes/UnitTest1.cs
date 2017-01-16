d2f72c018d796c3c67f5e31df0e9d801d85ea0ae
Richard J Byrne <richard.j.Byrne@gmail.com>
2017-01-16 22:45:40 +0000

    Started with Unit Tests

---
+++ UnitTests/Classes/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Haptic_Theatre_Vibings_Control.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haptic_Theatre_Vibings_Control.Classes.Tests
+{
+    [TestClass()]
+    public class UnitTest1
+    {
+        [TestMethod()]
+        public void SwitchDisplayBySensorValueTest()
+        {
+            ModeChangeTriggering.SwitchDisplayBySensorValue(40);
+        }
+    }
+}
